7d92135c0d05a49cebc2bf5df22399f189dcb0f7
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-28 12:35:10 +0300

    initial commit

--- Pdf Merge/PdfMergeApp.swift
+++ Pdf Merge/PdfMergeApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 @main
-struct Pdf_MergeApp: App {
+struct PdfMergeApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
---
+++ Pdf Merge/PdfMergeView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+
+struct PdfMergeView: View {
+
+    var body: some View {
+        VStack {
+            Text("Test")
+        }
+    }
+}
+
+#Preview {
+    PdfMergeView()
+        
+}
